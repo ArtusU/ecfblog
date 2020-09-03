2e38b4f7a628bdd68a589780fcaf302fe027c0b5
ArtusU <artusrock@hotmail.com>
2020-09-03 12:47:53 +0100

    model Post featured Field added

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import index, blog, post
+from posts.views import index, blog, post
 
 
 urlpatterns = [
