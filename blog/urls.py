eed1a59bfaa365a5d81f7089fe6caa7fcecec5f6
ArtusU <artusrock@hotmail.com>
2020-09-03 11:33:42 +0100

    post.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import index, blog
+from .views import index, blog, post
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog),
+    path('post/', post),
 ]
 
 if settings.DEBUG:
