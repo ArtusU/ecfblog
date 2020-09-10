ed0c58c2a4e61650798dfeadee3bf83a0c3c4da8
ArtusU <artusrock@hotmail.com>
2020-09-10 10:49:42 +0100

    post comment form

--- posts/forms.py
+++ posts/forms.py
@@ -19,3 +19,15 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'overview', 'content', 'thumbnail', 
         'categories', 'featured', 'previous_post', 'next_post')
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Type your comment',
+        'id': 'usercomment',
+        'rows': '3'
+    }))
+    class Meta:
+        model = Comment
+        fields = ('content',) 
