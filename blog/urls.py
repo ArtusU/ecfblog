7a1f56faed64ae04a1977745cae1ae98ab76465d
ArtusU <artusrock@hotmail.com>
2020-09-19 19:32:37 +0100

    class based views

--- blog/urls.py
+++ blog/urls.py
@@ -3,21 +3,54 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from posts.views import index, blog, post, search, post_create, post_update, post_delete 
+from posts.views import (
+    index, 
+    post_list, 
+    post_detail, 
+    search, 
+    post_create, 
+    post_update, 
+    post_delete,
+
+    IndexView,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    SearchView
+    )
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('blog/', blog, name='post-list'),
-    path('search/', search, name='search'),
-    path('post/<id>/', post, name='post-detail'),
-    path('create/', post_create, name='post-create'),
-    path('post/<id>/update', post_update, name='post-update'),
-    path('post/<id>/delete', post_delete, name='post-delete'),
+
+    path('', IndexView.as_view(), name='home'),
+
+    #path('blog/', post_list, name='post-list'),
+    path('blog/', PostListView.as_view(), name='post-list'),
+
+    #path('post/<id>/', post_detail, name='post-detail'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+
+
+    #path('create/', post_create, name='post-create'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
+
+    #path('post/<pk>/update', post_update, name='post-update'),
+    path('post/<pk>/update', PostUpdateView.as_view(), name='post-update'),
+
+    #path('post/<pk>/delete', post_delete, name='post-delete'),
+    path('post/<pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+
+    #path('search/', search, name='search'),
+    path('search/', SearchView.as_view(), name='search'),
+
     path('tinymce/', include('tinymce.urls')),
     path('accounts/', include('allauth.urls')),
-    path('/accounts/profile/', index),
+    path('accounts/profile/', index),
+    
     
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,35 @@
 from django.db.models import Count, Q
+from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from .forms import CommentForm, PostForm
 from .models import Post, Author, PostView
+from marketing.forms import EmailSignupForm
 from marketing.models import Signup
+
 
 def get_author(user):
     qs = Author.objects.filter().first()
     if qs:
         return qs
     return None
+
+
+class SearchView(View):
+    def get(self, request, *args, **kwargs):
+        queryset = Post.objects.all()
+        query = request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(overview__icontains=query)
+            ).distinct()
+        context = {
+            'queryset': queryset
+        }
+        return render(request, 'search_results.html', context)
 
 
 def search(request):
@@ -31,6 +51,28 @@ def get_category_count():
     return queryset
 
 
+class IndexView(View):
+    form = EmailSignupForm()
+
+    def get(self, request, *args, **kwargs):
+        featured = Post.objects.filter(featured=True).order_by('-timestamp')
+        latest = Post.objects.order_by('-timestamp')[0:3]
+        context = {
+            'object_list': featured,
+            'latest': latest,
+            'form': self.form
+        }
+        return render(request, 'index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email")
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+        messages.info(request, "Successfully subscribed")
+        return redirect("home")
+
+
 def index(request):
     featured = Post.objects.filter(featured=True).order_by('-timestamp')
     latest = Post.objects.order_by('-timestamp')[0:3]
@@ -48,7 +90,25 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def blog(request):
+class PostListView(ListView):
+    form = EmailSignupForm()
+    model = Post
+    template_name = 'blog.html'
+    context_object_name = 'queryset'
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.order_by('-timestamp')[:3]
+        context = super().get_context_data(**kwargs)
+        context['most_recent'] = most_recent
+        context['page_request_var'] = "page"
+        context['category_count'] = category_count
+        context['form'] = self.form
+        return context
+
+
+def post_list(request):
     category_count = get_category_count()
     #print(category_count)
     most_recent = Post.objects.order_by('-timestamp')[:3]
@@ -72,7 +132,44 @@ def blog(request):
     }
     return render(request, 'blog.html', context)
 
-def post(request, id):
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+    form = CommentForm()
+
+    def get_object(self):
+        obj = super().get_object()
+        if self.request.user.is_authenticated:
+            PostView.objects.get_or_create(user=self.request.user, post=obj)
+        return obj
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.order_by('-timestamp')[:3]
+        context = super().get_context_data(**kwargs)
+        context['most_recent'] = most_recent
+        context['page_request_var'] = "page"
+        context['category_count'] = category_count
+        context['form'] = self.form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post = self.get_object()
+            form.instance.user = request.user
+            form.instance.post = post
+            form.save()
+            return redirect(reverse("post-detail", kwargs={
+                'pk': post.pk
+            }))
+
+
+
+
+def post_detail(request, id):
     category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
     post = get_object_or_404(Post, id=id)
@@ -98,6 +195,23 @@ def post(request, id):
     return render(request, 'post.html', context)
 
 
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse("post-detail", kwargs={
+            'pk': form.instance.pk
+        }))
+
 
 def post_create(request):
     title = 'Create'
@@ -115,6 +229,24 @@ def post_create(request):
         'form': form
     }
     return render(request, "post_create.html", context)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse("post-detail", kwargs={
+            'pk': form.instance.pk
+        }))
 
 
 
@@ -139,6 +271,11 @@ def post_update(request, id):
     }
     return render(request, "post_create.html", context)
 
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog'
+    template_name = 'post_confirm_delete.html'
 
 
 def post_delete(request, id):
